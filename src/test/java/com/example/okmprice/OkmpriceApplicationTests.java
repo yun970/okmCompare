d937d2e76ccfe0ffffebc8e3f9f30139cbce274f
Yun970 <scc00057@gmail.com>
2023-09-03 16:09:13 +0900

    test

--- src/test/java/com/example/okmprice/OkmpriceApplicationTests.java
+++ src/test/java/com/example/okmprice/OkmpriceApplicationTests.java
@@ -14,32 +14,4 @@ import java.util.List;
 @SpringBootTest
 class OkmpriceApplicationTests {
 
-	@Autowired
-	ProductsService productsService;
-	@Autowired
-	BrandsService brandsService;
-	@Test
-	void contextLoads() {
-		List<Products> p = productsService.searchLowestPrice();
-		for(Products li : p){
-			System.out.printf(li.getProductNum()+" "+li.getBrands()+"\n");
-		}
-	}
-
-	@Test
-	void test(){
-		List<Products> p = productsService.searchCheapProducts();
-		for(Products li : p){
-			System.out.printf(li.getProductNum()+" "+li.getBrands()+"\n");
-		}
-	}
-
-	@Test
-	@Transactional
-	void test2(){
-		Brands brands=brandsService.searchBrands("니들스");
-		System.out.printf(brands.getName()+"\n");
-		System.out.printf(brands.getProducts().toString());
-	}
-
 }
